dbe22e84093b517e62c3c7e08ef74079b1350d17
Ruby Montes <ruby.montes.rosas@gmail.com>
2024-05-29 12:27:35 -0600

    first commit

---
+++ Almacen/DTOs/AreaDTO.cs
@@ -0,0 +1,11 @@
+﻿using Almacen.Entities;
+
+namespace Almacen.DTOs
+{
+    public class AreaDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        
+    }
+}
